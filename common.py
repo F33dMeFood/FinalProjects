805547c68ebb1b7fc967e7fb02032ab4c1d86538
F33dMeFood <32984791+F33dMeFood@users.noreply.github.com>
2017-12-02 13:20:23 -0600

    common.py
    code

---
+++ common.py
@@ -0,0 +1,33 @@
+'''
+Created on Dec 2, 2017
+
+@author: ITAUser
+'''
+def countingletters(filename, mychar):
+    f = open(filename, 'r')
+    count = 0;
+    run = True
+    while run:
+        letter = f.read(1)
+        letter = letter.lower()
+        if letter == "":
+            break
+        else:
+            if letter == mychar:
+                count = count + 1
+    print(count)
+    
+
+CountingLetters(filename="constitution.txt", mychar="a")
+CountingLetters(filename="constitution.txt", mychar="b")
+letters = list['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+for letter in letters:
+    currentcount = CountingLetters("constitution.txt", letter)
+    countlist.append(currentcount)
+print(countlist)
+dracula = max(countlist)
+print(dracula)
+indexletter = countlist.[indexlist]
+
+
+
